7a05669e7e56efa564d755d5fcc3f8cfddd82269
Vladimir <vova.kaverya@mail.ru>
2024-07-12 03:12:04 +0300

    pdf fix

--- src/main/java/com/events/controller/DocumentController.java
+++ src/main/java/com/events/controller/DocumentController.java
@@ -15,6 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpHeaders;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -102,7 +103,7 @@ public class DocumentController {
                 .orElseThrow(() -> new RuntimeException("Event not found"));
         List<Event_Member> members = eventService.findApprovedMembersByEventId(eventId,true);
         if (members.isEmpty()){
-            return ResponseEntity.ok()
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
                     .body("Участники мероприятия не найдены");
         }
 
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,9 +2,9 @@ INSERT INTO event_schema.users VALUES
                                      ('6c81ae12-7bac-4eab-ae1a-160743edbdaf','admin','$2a$04$Fx/SX9.BAvtPlMyIIqqFx.hLY2Xp8nnhpzvEEVINvVpwIPbA3v/.i');
 
 INSERT INTO event_schema.roles VALUES
-                                   ('d2dd39a5-b13c-426f-aa29-da82bb6dbf29','ADMIN'),
-                                   ('9506f129-5b3b-4b82-8202-75aefd1da713','ORGANIZER'),
-                                   ('f93d3f09-452a-4dc8-9b60-c3177242f069','USER');
+                                   ('d2dd39a5-b13c-426f-aa29-da82bb6dbf29','ROLE_ADMIN'),
+                                   ('9506f129-5b3b-4b82-8202-75aefd1da713','ROLE_ORGANIZATOR'),
+                                   ('f93d3f09-452a-4dc8-9b60-c3177242f069','ROLE_USER');
 
 INSERT INTO event_schema.events VALUES
                                    ('603b961f-a315-4c6e-9305-bea0ef60a98c','2024-07-07 17:12:19.146000','first_event','idk','6c81ae12-7bac-4eab-ae1a-160743edbdaf',true);
