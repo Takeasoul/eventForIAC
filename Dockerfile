04cd501417a6192d2e6544d666e84df668ff3bb8
Takeasoul <odenter2003@gmail.com>
2024-07-12 23:20:23 +0300

    DOCKER )))

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+# Используем базовый образ с установленным JDK
+FROM openjdk:17-jdk-slim AS build
+
+# Устанавливаем рабочую директорию
+WORKDIR /app
+
+# Копируем исполняемый JAR файл Maven
+COPY target/events-0.0.1-SNAPSHOT.jar app.jar
+
+# Открываем порт, на котором работает ваше приложение
+EXPOSE 8080
+
+# Определяем переменные окружения, если нужно
+ENV JAVA_OPTS=""
+
+# Команда для запуска приложения
+ENTRYPOINT ["java", "-jar", "app.jar"]
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,70 @@
+-- Создание схемы event_schema, если её нет
+CREATE SCHEMA IF NOT EXISTS event_schema;
+
+-- Установка владельца для схемы event_schema
+ALTER SCHEMA event_schema OWNER TO postgres;
+
+-- Создание таблицы event_members
+CREATE TABLE IF NOT EXISTS event_schema.event_members (
+                                                          id         UUID PRIMARY KEY NOT NULL,
+                                                          approved   BOOLEAN,
+                                                          clubmember BOOLEAN,
+                                                          company    VARCHAR(255),
+    email      VARCHAR(255),
+    event_id   UUID,
+    firstname  VARCHAR(255),
+    lastname   VARCHAR(255),
+    middlename VARCHAR(255),
+    phone      VARCHAR(255),
+    position   VARCHAR(255)
+    );
+
+-- Установка владельца для таблицы event_members
+ALTER TABLE event_schema.event_members OWNER TO postgres;
+
+-- Создание таблицы events
+CREATE TABLE IF NOT EXISTS event_schema.events (
+                                                   event_id      UUID PRIMARY KEY NOT NULL,
+                                                   event_date    TIMESTAMP,
+                                                   event_name    VARCHAR(255),
+    event_summary VARCHAR(255),
+    reg_open      BOOLEAN,
+    orgid         UUID,
+    address       VARCHAR(255)
+    );
+
+-- Установка владельца для таблицы events
+ALTER TABLE event_schema.events OWNER TO postgres;
+
+-- Создание таблицы roles
+CREATE TABLE IF NOT EXISTS event_schema.roles (
+                                                  id   UUID PRIMARY KEY NOT NULL,
+                                                  role VARCHAR(255)
+    );
+
+-- Установка владельца для таблицы roles
+ALTER TABLE event_schema.roles OWNER TO postgres;
+
+-- Создание таблицы users
+CREATE TABLE IF NOT EXISTS event_schema.users (
+                                                  id       UUID PRIMARY KEY NOT NULL,
+                                                  login    VARCHAR(255),
+    password VARCHAR(255)
+    );
+
+-- Установка владельца для таблицы users
+ALTER TABLE event_schema.users OWNER TO postgres;
+
+-- Создание таблицы users_roles
+CREATE TABLE IF NOT EXISTS event_schema.users_roles (
+                                                        user_id UUID REFERENCES event_schema.users(id),
+    role_id UUID REFERENCES event_schema.roles(id),
+    CONSTRAINT users_roles_pk PRIMARY KEY (user_id, role_id)
+    );
+
+-- Установка владельца для таблицы users_roles
+ALTER TABLE event_schema.users_roles OWNER TO postgres;
+
 INSERT INTO event_schema.users VALUES
                                      ('6c81ae12-7bac-4eab-ae1a-160743edbdaf','admin','$2a$04$Fx/SX9.BAvtPlMyIIqqFx.hLY2Xp8nnhpzvEEVINvVpwIPbA3v/.i');
 
@@ -7,7 +74,7 @@ INSERT INTO event_schema.roles VALUES
                                    ('f93d3f09-452a-4dc8-9b60-c3177242f069','ROLE_USER');
 
 INSERT INTO event_schema.events VALUES
-                                   ('603b961f-a315-4c6e-9305-bea0ef60a98c','2024-07-07 17:12:19.146000','first_event','idk','6c81ae12-7bac-4eab-ae1a-160743edbdaf',true);
+                                   ('603b961f-a315-4c6e-9305-bea0ef60a98c','2024-07-07 17:12:19.146000','first_event','idk',true,'6c81ae12-7bac-4eab-ae1a-160743edbdaf', 'sadasd');
 
 INSERT INTO event_schema.users_roles VALUES
                                    ('6c81ae12-7bac-4eab-ae1a-160743edbdaf','d2dd39a5-b13c-426f-aa29-da82bb6dbf29');
